f099efe51f0169b4f3019f86a49c13fc464a75de
JotasCode <223016397+JotasCode@users.noreply.github.com>
2025-12-08 13:01:21 -0400

    Add files via upload

---
+++ CRUD.py
@@ -0,0 +1,236 @@
+import os
+from supabase import create_client, Client
+from dotenv import load_dotenv
+load_dotenv()
+from new_id import new_id
+
+try:
+    url: str = os.environ.get('SUPABASE_URL')
+    key: str = os.environ.get('SUPABASE_KEY')
+    supabase: Client = create_client(url, key)
+except Exception as e:
+    print(f"Error al inicializar el cliente de Supabase: {e}")
+    exit(1)
+
+#FUNCIÓN PARA CREAR
+def create_data(table_name, table_id, table_column):
+    print(f'---------- Registrando fila en {table_name}----------')
+
+    if table_name != 'productos':
+        while True:
+            name = input('Nombre: ').strip()
+            if not name:
+                print('Debe insertar el nombre obligatoriamente.')
+            else:
+                break
+        
+        try:
+            supabase.table(table_name).insert({table_id: new_id(table_name, table_id),table_column: name}).execute()
+
+            print(f'Fila con el nombre {name} creada con éxito.')
+        except Exception as e:
+            print(f'Ocurrió un error al intentar crear la fila: {e}.')
+    else:
+        pass
+
+    if table_name == 'productos':
+        while True:
+            read_data('categorias')
+            category = int(input('ID de la categoría del producto: '))
+            if not category:
+                print('Debe insertar la categoría obligatoriamente.')
+            else: 
+                break
+
+        while True:
+            read_data('compañias')
+            company = int(input('ID de la compañía del producto: '))
+            if not company:
+                print('Debe insertar la compañía obligatoriamente.')
+            else: 
+                break
+
+        while True:
+            name = input('Nombre del producto: ').strip()
+            if not name:
+                print('Debe insertar el nombre obligatoriamente.')
+            else:
+                break
+
+        while True:
+            weight = int(input('Peso del producto (en gramos): '))
+            if not weight:
+                print('Debe insertar el peso obligatoriamente.')
+            else: 
+                break
+
+        while True:
+            stock = int(input('Cantidad del producto: '))
+            if not stock:
+                print('Debe insertar la cantidad obligatoriamente.')
+            else: 
+                break
+
+        while True:
+            exp_date = int(input('Fecha de expiración (YYMMDD): '))
+            if not exp_date:
+                print('Debe insertar la feca obligatoriamente.')
+            else: 
+                break
+        
+        try:
+            supabase.table(table_name).insert({table_id: new_id('productos', 'id_producto'),'id_categoria': category,'id_compañia': company, table_column: name, 'peso_producto_gramos': weight,'cantidad_stock': stock, 'fecha_exp': exp_date}).execute()
+
+            print(f'Producto {name} creado con éxito.')
+        except Exception as e:
+            print(f'Ocurrió un error al intentar crear el producto: {e}.')
+    else:
+        pass
+
+#FUNCIÓN PARA LEER
+def read_data(table_name):
+    print(f'---------- {table_name} ----------')
+
+    try:
+        data_select = supabase.table(table_name).select('*').execute()
+
+        if data_select.data == []:
+            print('No hay filas registradas.')
+        else:
+            print(data_select)
+    except Exception as e:
+        print(f'Ocurrió un error al intentar ver las filas registradas: {e}')
+
+#FUNCIÓN PARA ACTUALIZAR
+def update_data(table_name, table_id, table_column):
+    print(f'---------- Editando fila en {table_name} ----------')
+
+    if table_name != 'productos':
+        while True:
+            if table_name == 'compañias':
+                read_data('compañias')
+            if table_name == 'categorias':
+                read_data('categorias')
+            else:
+                pass
+
+            id_to_update = int(input('ID a editar: '))
+            if not id_to_update:
+                print('Debe insertar un id obligatoriamente.')
+            else:
+                break
+
+        while True: 
+            new_name = input('Nuevo nombre: ').strip()
+            if not new_name:
+                print('Debe insertar el nombre obligatoriamente.')
+            else:
+                break
+
+        try:
+            supabase.table(table_name).update({table_column: new_name}).eq(table_id, id_to_update).execute()
+
+            print(f'Fila con el ID: {id_to_update} editado con éxito.')
+        except Exception as e:
+            print(f'Ocurrió un error al intentar editar la fila: {e}.')
+    else:
+        pass
+
+    if table_name == 'productos':
+        while True:
+            read_data('productos')
+            products_id_to_update = int(input('ID del producto: '))
+            if not products_id_to_update:
+                print('Debe insertar un id obligatoriamente.')
+            else:
+                break
+        
+        while True:
+            read_data('categorias')
+            new_category = int(input('ID de la nueva categoría del producto: '))
+            if not new_category:
+                print('Debe insertar la categoría obligatoriamente.')
+            else: 
+                break
+
+        while True:
+            read_data('compañias')
+            new_company = int(input('ID de la nueva compañía del producto: '))
+            if not new_company:
+                print('Debe insertar la compañía obligatoriamente.')
+            else: 
+                break
+
+        while True: 
+            new_name = input('Nuevo nombre del producto: ').strip()
+            if not new_name:
+                print('Debe insertar el nombre obligatoriamente.')
+            else:
+                break
+
+        while True:
+            new_weight = int(input('Nuevo peso del producto (en gramos): '))
+            if not new_weight:
+                print('Debe insertar el peso obligatoriamente.')
+            else:
+                break
+        
+        while True:
+            new_stock = int(input('Nueva cantidad del producto: '))
+            if not new_stock:
+                print('Debe insertar la cantidad obligatoriamente.')
+            else: 
+                break
+        
+        while True:
+            new_exp_date = int(input('Nueva fecha de expiración (YYMMDD): '))
+            if not new_exp_date:
+                print('Debe insertar la fecha obligatoriamente.')
+            else: 
+                break
+        try:
+            supabase.table(table_name).update({'id_categoria': new_category,'id_compañia': new_company, table_column: new_name, 'peso_producto_gramos': new_weight,'cantidad_stock': new_stock,'fecha_exp': new_exp_date}).eq(table_id, products_id_to_update).execute()
+
+            print(f'Producto con el ID: {products_id_to_update} editado con éxito.')
+        except Exception as e:
+            print(f'Ocurrió un error al intentar editar el producto: {e}.')
+    else:
+        pass
+
+#FUNCIÓN PARA ELIMINAR
+def delete_data(table_name, table_id):
+    print(f'---------- Eliminando fila en {table_name} ----------')
+
+    while True:
+        check = input(f'¿Esta seguro(a) de querer eliminar una fila de {table_name}? (si/no) ')
+
+        if check == 'no':
+            print('Operación cancelada.')
+            return
+        elif check == 'si':
+            break
+        else:
+            print('Respuesta no válida. Por favor introduzca "si" o "no".')
+
+    while True:
+        id_to_delete = input('ID a eliminar: ')
+
+        if not id_to_delete:
+            print('Debe insertar un id obligatoriamente.')
+        else:
+            while True:
+                double_check = input(f'¿Esta seguro(a) de querer elilminar esta fila de {table_name}? (si/no) ')
+
+                if double_check == 'no':
+                    print('Doble verificación cancelada. Volviendo a la entrada del ID.')
+                    break
+                elif double_check == 'si':
+                    try:
+                        supabase.table(table_name).delete().eq(table_id, id_to_delete).execute()
+
+                        print(f'Fila con el ID: {id_to_delete} en {table_name} eliminada con éxito.')
+                    except Exception as e:
+                         print(f'Ocurrió un error al intentar eliminar esta fila: {e}.')
+                    return
+                else:
+                   print('Respuesta no válida. Por favor, introduzca "si" o "no".')
---
+++ main.py
@@ -0,0 +1,135 @@
+import os
+from CRUD import create_data, read_data, update_data, delete_data
+from to_csv import from_table_to_csv as to_csv
+from dotenv import load_dotenv
+load_dotenv()
+
+def menu():
+    print('-' * 60)
+    print('Programa de registro de inventario.')
+
+    while True:
+        print('---------- Tablas ----------')
+        print('-' * 60)
+        print('1. Compañías')
+        print('2. Categorías')
+        print('3. Productos')
+        print('-' * 60)
+        print('4. Salir.')
+        print('-' * 60)
+
+        selection = input('Por favor seleccione una tabla: ')
+
+        if selection == '1':
+            while True:
+                print('---------- Elegiste la tabla de Compañías ----------')
+                print('1. Registrar compañía.')
+                print('2. Ver compañías registradas.')
+                print('3. Editar registro de compañía.')
+                print('4. Eliminar compañía.')
+                print('5. Guardar compañias en un archvo local.')
+                print('-' * 60)
+                print('6. Salir.')
+                print('-' * 60)
+
+                companys_selection = input('Por favor seleccione una opción: ')
+
+                if companys_selection == '1':
+                    create_data(os.environ.get('TABLE_1'), os.environ.get('TABLE_1_ID'), os.environ.get('TABLE_1_COLUMN'))
+                elif companys_selection == '2':
+                    read_data(os.environ.get('TABLE_1'))
+                elif companys_selection == '3':
+                    update_data(os.environ.get('TABLE_1'), os.environ.get('TABLE_1_ID'), os.environ.get('TABLE_1_COLUMN'))
+                elif companys_selection == '4':
+                    delete_data(os.environ.get('TABLE_1'), os.environ.get('TABLE_1_ID'))
+                elif companys_selection == '5':
+                    to_csv(os.environ.get('TABLE_1'), os.environ.get('TABLE_1_FILE'))
+                elif companys_selection == '6':
+                    print('-' * 60)
+                    print('Saliendo del registro de compañías.')
+                    print('-' * 60)
+                    break
+                else:
+                    print('-' * 60)
+                    print('Opción invalida. Seleccione el número de la opción que desea elegir.')
+
+        elif selection == '2':
+            while True:
+                print('---------- Elegiste la tabla de Categorías ----------')
+                print('1. Registrar categoría.')
+                print('2. Ver categorías registradas.')
+                print('3. Editar registro de categoría.')
+                print('4. Eliminar categoría.')
+                print('5. Guardar categorías en un archvo local.')
+                print('-' * 60)
+                print('6. Salir.')
+                print('-' * 60)
+
+                categorys_selection = input('Por favor seleccione una opción: ')
+
+                if categorys_selection == '1':
+                    create_data(os.environ.get('TABLE_2'), os.environ.get('TABLE_2_ID'), os.environ.get('TABLE_2_COLUMN'))
+                elif categorys_selection == '2':
+                    read_data(os.environ.get('TABLE_2'))
+                elif categorys_selection == '3':
+                    update_data(os.environ.get('TABLE_2'), os.environ.get('TABLE_2_ID'), os.environ.get('TABLE_2_COLUMN'))
+                elif categorys_selection == '4':
+                    delete_data(os.environ.get('TABLE_2'), os.environ.get('TABLE_2_ID'))
+                elif categorys_selection == '5':
+                    to_csv(os.environ.get('TABLE_2'), os.environ.get('TABLE_2_FILE'))
+                elif categorys_selection == '6':
+                    print('-' * 60)
+                    print('Saliendo del registro de categorías.')
+                    print('-' * 60)
+                    break
+                else:
+                    print('-' * 60)
+                    print('Opción invalida. Seleccione el número de la opción que desea elegir.')
+
+        elif selection == '3':
+            while True:
+                print('---------- Elegiste la tabla de Productos ----------')
+                print('1. Registrar producto.')
+                print('2. Ver productos registrados.')
+                print('3. Editar registro.')
+                print('4. Eliminar producto.')
+                print('5. Guardar productos en un archvo local.')
+                print('-' * 60)
+                print('6. Salir.')
+                print('-' * 60)
+
+                products_selection = input('Por favor seleccione una opción: ')
+
+                if products_selection == '1':
+                    create_data(os.environ.get('TABLE_3'), os.environ.get('TABLE_3_ID'), os.environ.get('TABLE_3_COLUMN'))
+                elif products_selection == '2':
+                    read_data(os.environ.get('TABLE_3'))
+                elif products_selection == '3':
+                    update_data(os.environ.get('TABLE_3'), os.environ.get('TABLE_3_ID'), os.environ.get('TABLE_3_COLUMN'))
+                elif products_selection == '4':
+                    delete_data(os.environ.get('TABLE_3'), os.environ.get('TABLE_3_ID'))
+                elif products_selection == '5':
+                    to_csv(os.environ.get('TABLE_3'), os.environ.get('TABLE_3_FILE'))
+                elif products_selection == '6':
+                    print('-' * 60)
+                    print('Saliendo del registro de productos.')
+                    print('-' * 60)
+                    break
+                else:
+                    print('-' * 60)
+                    print('Opción invalida. Seleccione el número de la opción que desea elegir.')
+
+        elif selection == '4':
+            print('-' * 60)
+            print('Gracias por utilizar nuestros servicios, hasta la próxima.')
+            print('-' * 60)
+            break
+
+        else:
+            print('-' * 60)
+            print('Opción invalida. Seleccione el número de la tabla que desea elegir.')
+
+if __name__ == '__main__':
+    menu()
+else:
+    print('No puedes ejecutar este programa.')
