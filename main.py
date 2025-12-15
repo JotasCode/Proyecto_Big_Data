9f5fcd0b9eff866d7445715effdb9c3274c2236d
JotasCode <223016397+JotasCode@users.noreply.github.com>
2025-12-15 12:15:32 -0400

    Add files via upload

--- main.py
+++ main.py
@@ -19,6 +19,7 @@ def menu():
         print('-' * 60)
 
         selection = input('Por favor seleccione una tabla: ')
+        selection_name = None
 
         if selection == '1':
             selection = 'compañía'
@@ -41,39 +42,6 @@ def menu():
             selection_id = 'TABLE_3_ID'
             selection_column = 'TABLE_3_COLUMN_1'
 
-        elif selection is None:
-            while True:
-                print(f'---------- Elegiste la tabla de {selection_name} ----------')
-                print(f'1. Registrar {selection}.')
-                print(f'2. Ver registro de {selection_name}.')
-                print(f'3. Editar registro de {selection}.')
-                print(f'4. Eliminar {selection}.')
-                print(f'5. Guardar registro de {selection_name} en un archvo local.')
-                print('-' * 60)
-                print('6. Salir.')
-                print('-' * 60)
-
-                selection_2 = input('Por favor seleccione una opción: ')
-
-                if selection_2 == '1':
-                    create_data(os.environ.get(selection_table), os.environ.get(selection_id), os.environ.get(selection_column))
-                elif selection_2 == '2':
-                    read_data(os.environ.get(selection_table))
-                elif selection_2 == '3':
-                    update_data(os.environ.get(selection_table), os.environ.get(selection_id), os.environ.get(selection_column))
-                elif selection_2 == '4':
-                    delete_data(os.environ.get(selection_table), os.environ.get(selection_id))
-                elif selection_2 == '5':
-                    to_csv(os.environ.get(selection_table))
-                elif selection_2 == '6':
-                    print('-' * 60)
-                    print(f'Saliendo del registro de {selection_name}.')
-                    print('-' * 60)
-                    break
-                else:
-                    print('-' * 60)
-                    print('Opción invalida. Seleccione el número de la opción que desea elegir.')
-
         elif selection == '4':
             print('-' * 60)
             print('Gracias por utilizar nuestros servicios, hasta la próxima.')
@@ -82,7 +50,45 @@ def menu():
 
         else:
             print('-' * 60)
-            print('Opción invalida. Seleccione el número de la opción que desea elegir.')
+            print('Opción inválida. Seleccione un número del 1 al 4.')
+            continue 
+
+        while True:
+            print(f'---------- Elegiste la tabla de {selection_name} ----------')
+            print(f'1. Registrar {selection}.')
+            print(f'2. Ver registro de {selection_name}.')
+            print(f'3. Editar registro de {selection}.')
+            print(f'4. Eliminar {selection}.')
+            print(f'5. Guardar registro de {selection_name} en un archivo local.')
+            print('-' * 60)
+            print('6. Salir.')
+            print('-' * 60)
+
+            selection_2 = input('Por favor seleccione una opción: ')
+
+            if selection_2 == '1':
+                create_data(os.environ.get(selection_table),
+                            os.environ.get(selection_id),
+                            os.environ.get(selection_column))
+            elif selection_2 == '2':
+                read_data(os.environ.get(selection_table))
+            elif selection_2 == '3':
+                update_data(os.environ.get(selection_table),
+                            os.environ.get(selection_id),
+                            os.environ.get(selection_column))
+            elif selection_2 == '4':
+                delete_data(os.environ.get(selection_table),
+                            os.environ.get(selection_id))
+            elif selection_2 == '5':
+                to_csv(os.environ.get(selection_table))
+            elif selection_2 == '6':
+                print('-' * 60)
+                print(f'Saliendo del registro de {selection_name}.')
+                print('-' * 60)
+                break
+            else:
+                print('-' * 60)
+                print('Opción inválida. Seleccione el número de la opción que desea.')
 
 if __name__ == '__main__':
     menu()
